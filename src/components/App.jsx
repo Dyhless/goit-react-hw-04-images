b3c80fdb66a4e6e7ad01f93f4dde76fa9fc14ac9
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-24 21:48:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,5 +6,66 @@ import Loader from './Loader/Loader';
 import Searchbar from './Searchbar/Searchbar';
 
 export const App = () => {
-  const 
-}
+  const [searchText, setSearchText] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
+  };
+
+  handleSubmit = query => {
+    this.setState({
+      searchText: query,
+      images: [],
+      currentPage: 1,
+    });
+  };
+
+  addImages = async () => {
+    const { searchText, currentPage } = this.state;
+    try {
+      this.setState({ isLoading: true });
+      const data = await API.getImages(searchText, currentPage);
+
+      if (data.hits.length === 0) {
+        return alert('No such images');
+      }
+
+      const imagesFormatedtoList = data.hits.map(
+        ({ id, tags, webformatURL, largeImageURL }) => ({
+          id,
+          tags,
+          webformatURL,
+          largeImageURL,
+        })
+      );
+
+      this.setState(state => ({
+        images: [...state.images, ...imagesFormatedtoList],
+        error: '',
+        totalPages: Math.ceil(data.totalHits / 12),
+      }));
+    } catch (error) {
+      this.setState({ error: 'Sorry, some error' });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {images.length > 0 && <ImageGallery images={images} />}
+      {isLoading && <Loader />}
+      {images.length > 0 && totalPages !== currentPage && !isLoading && (
+        <Button onClick={loadMore} />
+      )}
+    </>
+  );
+};
