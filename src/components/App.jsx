afbb3e51c5f1e70c9545a4cb191ae848f77993f4
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-25 17:29:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import * as API from './getImages';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
@@ -10,24 +10,9 @@ export const App = () => {
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [totalPages, setTotalPages] = useState(0);
 
-  useEffect(() => {
-    addImages();
-  }, [searchText, currentPage]);
-
-  const loadMore = () => {
-    setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
-  };
-
-  const handleSubmit = query => {
-    setSearchText(query);
-    setImages([]);
-    setCurrentPage(1);
-  };
-
-  const addImages = async () => {
+  const addImages = useCallback(async () => {
     try {
       setIsLoading(true);
       const data = await API.getImages(searchText, currentPage);
@@ -46,13 +31,26 @@ export const App = () => {
       );
 
       setImages(prevImages => [...prevImages, ...imagesFormattedToList]);
-      setError('');
       setTotalPages(Math.ceil(data.totalHits / 12));
     } catch (error) {
-      setError('Sorry, some error');
+      console.error('Sorry, some error', error);
     } finally {
       setIsLoading(false);
     }
+  }, [searchText, currentPage]);
+
+  useEffect(() => {
+    addImages();
+  }, [addImages]);
+
+  const loadMore = () => {
+    setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
+  };
+
+  const handleSubmit = query => {
+    setSearchText(query);
+    setImages([]);
+    setCurrentPage(1);
   };
 
   return (
